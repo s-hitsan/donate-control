91207ad39687d8634d46291424b933be5dacae0b
Steele Valles <steele.valles@kiv.dev>
2024-06-24 21:50:52 +0300

    navigation added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageSourcePropType } from 'react-native';
+import { Tabs } from 'expo-router';
+import { tabs } from '@/constants';
 
+type TabBarIconProps = {
+  icon: ImageSourcePropType;
+  label: string;
+  focused: boolean;
+  color: string;
+};
+
+const TabBarIcon = (props: TabBarIconProps) => {
+  const { icon, label, focused, color } = props;
+  return (
+    <View className="items-center justify-center gap-0.5">
+      <Image resizeMode="contain" className="w-4 h-4" tintColor={color} source={icon} />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>{label}</Text>
+    </View>
+  );
+};
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}>
+      {tabs.map((tab, index) => (
+        <Tabs.Screen
+          key={tab.path}
+          name={tab.path}
+          options={{
+            title: tab.label,
+            headerShown: false,
+            tabBarIcon: props => TabBarIcon({ icon: tab.icon, label: tab.label, ...props }),
+          }}
+        />
+      ))}
+    </Tabs>
   );
 };
 
